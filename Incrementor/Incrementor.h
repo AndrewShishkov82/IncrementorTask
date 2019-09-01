369a586762b4f0fb8a6a85f7cf2bce0871be9ae0
andrew shishkov <ashishkov@accesssoftek.com>
2019-09-01 17:15:14 +0300

    Initial Commit

---
+++ Incrementor/Incrementor.h
@@ -0,0 +1,19 @@
+//
+//  Incrementor.h
+//  Incrementor
+//
+//  Created by andrew shishkov on 9/1/19.
+//  Copyright © 2019 andrew shishkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Incrementor.
+FOUNDATION_EXPORT double IncrementorVersionNumber;
+
+//! Project version string for Incrementor.
+FOUNDATION_EXPORT const unsigned char IncrementorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Incrementor/PublicHeader.h>
+
+
